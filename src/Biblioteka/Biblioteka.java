64db8b667061c99417b6f7f0eb14a63eefb97b92
Bibesko <bibesko94@gmail.com>
2015-03-17 12:58:32 +0100

    Dodata logicka kontorola u sve klase Biblioteka.

--- src/Biblioteka/Biblioteka.java
+++ src/Biblioteka/Biblioteka.java
@@ -10,12 +10,26 @@ public class Biblioteka implements Bibliotekainterfejs {
 	
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
+		
+		if ( knjiga == null) {
+			throw new RuntimeException("Knjiga ne sme biti null.");
+		}
+		
+		if ( knjige.contains(knjiga)) {
+			throw new RuntimeException ("Knjiga je vec u biblioteci.");
+		}
+		
 		knjige.add(knjiga);
 
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga knjiga) {
+		
+		if ( !knjige.contains(knjiga)) {
+			throw new RuntimeException ("Knjiga ne postoji u biblioteci.");
+		}
+				
 		knjige.remove(knjiga);
 
 	}
@@ -25,6 +39,10 @@ public class Biblioteka implements Bibliotekainterfejs {
 			String izdavac, Autor autor) {
 		
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
+		
+		if ( naslov == null && izdavac == null && autor == null ){
+			throw new RuntimeException("Morate uneti neki kriterijum.");
+		}
 		
 		if ( ISBN > 0 ) {
 			for (int i = 0; i < knjige.size(); i++) {
